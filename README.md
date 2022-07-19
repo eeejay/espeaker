ddb08bf908ffcc9724aefeafe042a84ec05d4613
Eitan Isaacson <eitan@monotonous.org>
2022-07-18 21:49:09 -0700

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# eSpeak NG playback library
+
+This library provides a `rodio::Source` that can be used to output generate eSpeak audio. It supports listing and setting voices, triggering a callback for boundary or marker events, and full control of other eSpeak parameters like rate and pitch. See example and tests for usage.
+
+## License
+[License]: #license
+
+Licensed under ([MIT license](LICENSE)).
---
+++ examples/main.rs
@@ -0,0 +1,59 @@
+use espeaker;
+use rodio::{OutputStream, Sink};
+
+fn main() {
+    let s = String::from(
+        "I was going to smoke the marijuana like a cigarette. I shall hide behind the couch.",
+    );
+    let mut speaker = espeaker::Speaker::new();
+    speaker.params.pitch = Some(400);
+    let source = speaker.speak(&s);
+    let source = source.with_callback(move |evt| match evt {
+        espeaker::Event::Word(start, len) => {
+            println!("'{}'", &s[..][start..(start + len)]);
+        }
+        espeaker::Event::Sentence(_) => (),
+    });
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
+
+    let s = String::from(
+        "I was going to smoke the marijuana like a cigarette. I shall hide behind the couch.",
+    );
+    let mut speaker = espeaker::Speaker::new();
+    speaker.params.rate = Some(280);
+    let source = speaker.speak(&s);
+    let source = source.with_callback(move |evt| match evt {
+        espeaker::Event::Word(start, len) => {
+            println!("'{}'", &s[..][start..(start + len)]);
+        }
+        espeaker::Event::Sentence(_) => (),
+    });
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
+
+    let s = String::from("كنت سأدخن الماريجوانا مثل السيجارة. سأختبئ خلف الأريكة.");
+    let mut speaker = espeaker::Speaker::new();
+
+    let voices = espeaker::list_voices();
+    let voice = voices.into_iter().find(|v| v.name == "Arabic").unwrap();
+    speaker.set_voice(&voice);
+    let source = speaker.speak(&s);
+    let source = source.with_callback(move |evt| match evt {
+        espeaker::Event::Word(start, len) => {
+            println!("{} {}", start, len);
+        }
+        espeaker::Event::Sentence(_) => (),
+    });
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
+}
